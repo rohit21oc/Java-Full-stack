095d3c4af82cfa6357a130642f343b5b57e441ab
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-10-11 00:00:03 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,15 @@
+public class assignment {
+    public static void main(String[] args) {
+        int a;
+        a=10;
+        System.out.println(a);
+        a+=10;
+        System.out.println(a);
+        a-=10;
+        System.out.println(a);
+        a*=10;
+        System.out.println(a);
+        a/=10;
+        System.out.println(a);
+    }
+}
