4b2f0e4de0a5ea05dbdf28407378b437e1093a40
ROHIT KUMAR <119073894+rohit21oc@users.noreply.github.com>
2024-10-10 23:58:23 +0530

    Add files via upload

---
+++ add3Numer.java
@@ -0,0 +1,10 @@
+public class add3Numer {
+    public static void main(String[] args) {
+        System.out.println("sum of these number is: ");
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = 7;
+        int sum = num1 +  num2 + num3;
+        System.out.println(sum);
+    }
+}
